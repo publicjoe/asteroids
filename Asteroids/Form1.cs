10afeed27e23a1e55c16ab22347eb98dfd807dfc
Michael Kitchen <38534817+publicjoe@users.noreply.github.com>
2022-11-16 20:22:34 +0000

    Tidy up

--- Asteroids/Form1.cs
+++ Asteroids/Form1.cs
@@ -1,26 +1,3 @@
-/*
- * Downloaded from www.publicjoe.co.uk
- *
- * This software is provided 'as-is', without any express or implied warranty.
- * In no event will the author(s) be held liable for any damages arising from
- * the use of this software.
- * 
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely.
- * 
- * This class is based on Michael Lambert's code Copyright 2000. Which is Based 
- * on Asteroids, Copyright 1998 by Mike Hall. All Rights Reserved.
- * 
- * Keyboard Controls:
- * 
- * S            - Start Game        P           - Pause Game
- * Cursor Left  - Rotate Left       Cursor Up   - Fire Thrusters
- * Cursor Right - Rotate Right      Cursor Down - Fire Retro Thrusters
- * Spacebar     - Fire Cannon       H           - Hyperspace
- * M            - Toggle Sound      D           - Toggle Graphics Detail
- */
-
 using Publicjoe.Windows;
 using System;
 using System.Diagnostics;
